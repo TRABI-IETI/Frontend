ae637d218e63853443fca1132a246976af2b2b04
nvmg <nicole.montana@mail.escuelaing.edu.co>
2023-04-16 01:22:09 -0500

    primera versión

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { Carousel } from 'react-responsive-carousel';
 import { Flex, Heading, Card, Box, Image, Text } from '@chakra-ui/react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Home from './pages/Home';
+import Paquetes from './pages/Paquetes';
 
 function App() {
   
   
   return (
-    <Home/>
+    <Box bg='linear-gradient(to bottom, #20cbc7, #86ce80)'>
+      <Paquetes/>
+    </Box>
 
   );
 }
