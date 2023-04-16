2eff104f3f1a1869bb4686360a1946c48f87477e
ser0710 <serroz2822@gmail.com>
2023-04-16 15:24:40 -0500

    localStorage

--- src/hooks/memoryHook.js
+++ src/hooks/memoryHook.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
 
+if(!localStorage.getItem("packages")){
+    localStorage.setItem("packages", JSON.stringify([]));
+  }
+
 export function memoryHook(){
-    const [packages, setPackages] = useState(JSON.parse(localStorage.getItem("tasks")));
+    const [packages, setPackages] = useState(JSON.parse(localStorage.getItem("packages")));
 
     function addPackage(packag){
-        console.log(packag)
-        addPack = [...packages, packag]
-        setPackages(packag)
         console.log(packages)
+        //console.log(packag)
+        let addPack = [...packages, packag]
+        setPackages(addPack)
+        localStorage.setItem("packages", JSON.stringify(addPack))
+        
     }
 
     return [packages, addPackage]
