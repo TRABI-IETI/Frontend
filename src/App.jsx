ab79813f5a7ef5b703fff0948d7e28dab01d51a7
camilo <camiloa@innfuturum.com>
2023-04-16 01:28:39 -0500

    Listado de lugares del paquete

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { Carousel } from 'react-responsive-carousel';
 import { Flex, Heading, Card, Box, Image, Text } from '@chakra-ui/react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Home from './pages/Home';
+import LugarDetailPage from './pages/LugarDetailPage';
 
 function App() {
   
   
   return (
-    <Home/>
+    <Box bg="#86ce80">
+      <LugarDetailPage/>
+    </Box>
 
   );
 }
