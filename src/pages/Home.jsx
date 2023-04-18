10f1df1d469245aceddf7d22b8a784c26f7c3a20
derjasai <elholaquehace88@gmail.com>
2023-04-17 21:11:24 -0500

    corrección de imagen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ import { usePlaceHook } from '../hooks/usePlaceHook';
 const items = [
     {
       image:
-        "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
         title: 'Card 3', 
         description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     },
@@ -94,7 +94,6 @@ function Home() {
     return (
       <Flex justifyContent= "center" alignItems= "center" flexDirection="column">
             <Usuario/>
-            <button onClick={()=>console.log(lugares)}>hola</button>
             <Heading textAlign={"center"}>TRABI</Heading>
         <Box
       position="relative"
@@ -112,7 +111,7 @@ function Home() {
                 <Link to={`/descripcionPaquete/${item.id}`} style={{textDecoration: 'none'}}>
                 <Card boxShadow="20px 20px 80px rgba(0, 0, 0, 0.2)"  borderRadius="md" p={7}>
 
-                  <Image src={handleIamage} alt={item.name}/>
+                  <Image src={handleIamage()} alt={item.name}/>
 
                   <Box p={4}>
                     <Heading size="md">{item.name}</Heading>
